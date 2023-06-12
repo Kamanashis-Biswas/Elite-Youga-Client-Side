c5919b817098902d4aa81cdae82e26166f194601
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2023-06-13 00:10:43 +0600

    Popular Instructors component add

---
+++ src/components/Popular Instructors/PopularInstructors.jsx
@@ -0,0 +1,12 @@
+import SectionTitle from "../SectionTitle/SectionTitle";
+
+
+const PopularInstructors = () => {
+    return (
+        <div>
+            <SectionTitle heading={'Popular Instructors'}></SectionTitle>
+        </div>
+    );
+};
+
+export default PopularInstructors;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet-async";
 import Banner from "../Banner/Banner";
 import PopularClasses from "../PopularClasses/PopularClasses";
 import Routine from "../Routine/Routine";
+import PopularInstructors from "../../../components/Popular Instructors/PopularInstructors";
 
 
 
@@ -12,10 +13,11 @@ const Home = () => {
                 <title>EliteYoga | Home</title>
             </Helmet>
             <Banner></Banner>
-            <div>
+           
                 <PopularClasses></PopularClasses>
-            </div>
+         
             <Routine></Routine>
+            <PopularInstructors></PopularInstructors>
         </div>
     );
 };
