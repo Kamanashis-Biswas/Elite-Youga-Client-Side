56e00784050f464dfeb969b5bda6444100927a80
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2023-06-12 13:44:52 +0600

    Add Class and My class Edit

--- src/pages/AddClass/AddClass.jsx
+++ src/pages/AddClass/AddClass.jsx
@@ -1,12 +1,15 @@
 import { useContext } from "react";
 import { AuthContext } from "../../components/providers/AuthProvider";
+import Swal from "sweetalert2";
 
 
 const AddClass = () => {
     const { user } = useContext(AuthContext);
     const handleSubmit = async (event)=>{
       event.preventDefault();
+      const form = event.target;
       const data = {
+        
         class_name: event.target.class_name.value,
         class_image: event.target.class_image.value,
         inst_name: event.target.inst_name.value,
@@ -20,6 +23,16 @@ const AddClass = () => {
       );
       const d = await resp.json();
       console.log(d);
+      if(d.cls.insertedId){
+        form.reset();
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'Class Added',
+          showConfirmButton: false,
+          timer: 1500
+      });
+      }
         
     };
     return (
@@ -32,13 +45,13 @@ const AddClass = () => {
                   <label className="label">
                     <span className="label-text">Class Name</span>
                   </label>
-                  <input name="class_name" type="text" placeholder="type here" className="input input-bordered" />
+                  <input name="class_name" required type="text" placeholder="type here" className="input input-bordered" />
                 </div>
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Class Image</span>
                   </label>
-                  <input name="class_image" type="text" placeholder="type here" className="input input-bordered" />
+                  <input name="class_image" required type="text" placeholder="type here" className="input input-bordered" />
                 </div>
                 <div className="form-control">
                   <label className="label">
@@ -56,13 +69,13 @@ const AddClass = () => {
                   <label className="label">
                     <span className="label-text">Available Seats</span>
                   </label>
-                  <input type="number" name="seats" placeholder="type here" className="input input-bordered" />
+                  <input type="number" required name="seats" placeholder="type here" className="input input-bordered" />
                 </div>
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Price</span>
                   </label>
-                  <input type="number" name="price" placeholder="type here" className="input input-bordered" />
+                  <input type="number" required name="price" placeholder="type here" className="input input-bordered" />
                 </div>
                 <div className="form-control mt-6">
                   <input className="btn btn-primary" type="submit" value="Add Class" />
--- src/pages/MyClasses/MyClasses.jsx
+++ src/pages/MyClasses/MyClasses.jsx
@@ -21,14 +21,14 @@ const MyClasses = () => {
            <SectionTitle heading={"My Classes Page"}></SectionTitle>
            <div className="grid grid-cols-1 md:grid-cols-2 gap-5 my-10 justify-center">
                 {data.map(d=>(
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure><img src="https://i0.wp.com/calmatters.org/wp-content/uploads/2021/08/class-size.jpg?fit=2266%2C1322&ssl=1" alt="Shoes" /></figure>
+                <div key={d._id} className="card w-96 bg-base-100 shadow-xl">
+                    <figure><img src={d.class_image} alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">
                             {d.class_name}
                             <div className="badge badge-secondary">Status</div>
                         </h2>
-                        <p>{d.seats}</p>
+                        <p>Avaiable Seat: {d.seats}</p>
                         <div className="flex gap-10">
                         <button className="btn btn-sm">Feedback</button>
                         <button className="btn btn-sm">Update</button>
