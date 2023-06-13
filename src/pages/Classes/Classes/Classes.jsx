5a9009482dcc4c19c41293e905d8ca3ffc2fe80d
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2023-06-13 12:05:32 +0600

    class and instructor page edit

--- src/pages/Classes/Classes/Classes.jsx
+++ src/pages/Classes/Classes/Classes.jsx
@@ -1,20 +1,33 @@
-import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { useEffect, useState } from "react";
 
 
 const Classes = () => {
+
+    const [cls, setCls] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/all-class').then(data=>data.json())
+            .then(data=>{
+                console.log(data.data);
+                setCls(data.data);
+            });
+    }, []);
     return (
-        <div>
-            <SectionTitle heading={"Class List"}></SectionTitle>
-            <div className="card w-96 bg-base-100 shadow-xl mb-10">
-                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+        <div className="grid grid-cols-3">
+            {cls.map(c=>(
+                <div key={c._id} className="card w-96 bg-base-100 shadow-xl mb-10">
+                    <figure><img src={c.class_image} alt="Class Image" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{c.class_name}</h2>
+                        <p>Instructor Name: {c.inst_name}</p>
+                        <p>Available Seat: {c.seats}</p>
+                        <p>Price: {c.price}</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
                     </div>
                 </div>
-            </div>
+            ))}
         </div>
     );
 };
--- src/pages/Instructors/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors/Instructors.jsx
@@ -1,10 +1,17 @@
-import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { useEffect, useState } from "react";
 
 
 const Instructors = () => {
+    const [inst, setInst] = useState([]);
+    useEffect(()=>{
+        fetch('http://localhost:5000/instructors').then((data)=>data.json())
+            .then(data=>{
+                console.log(data);
+                setInst(data || []);
+            });
+    }, []);
     return (
         <div>
-            <SectionTitle heading={"List of Instructors"}></SectionTitle>
             <div className="overflow-x-auto p-4 mb-10 border rounded-lg w-1/2 mx-auto">
                 <table className="table">
                     {/* head */}
@@ -15,48 +22,26 @@ const Instructors = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {/* row 1 */}
-                        <tr>
-                            <td>
-                                <div className="flex items-center space-x-3">
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle w-12 h-12">
-                                            <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                        {inst.map(i=>(
+                            <tr key={i._id}>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">{i.name}</div>
                                         </div>
                                     </div>
-                                    <div>
-                                        <div className="font-bold">Hart Hagerty</div>
-                                        <div className="text-sm opacity-50">United States</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                Zemlak, Daniel and Leannon
-                                <br />
-                                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-                            </td>
-                        </tr>
-                        {/* row 2 */}
-                        <tr>
-                            <td>
-                                <div className="flex items-center space-x-3">
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle w-12 h-12">
-                                            <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div className="font-bold">Hart Hagerty</div>
-                                        <div className="text-sm opacity-50">United States</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                Zemlak, Daniel and Leannon
-                                <br />
-                                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-                            </td>
-                        </tr>
+                                </td>
+                                <td>
+                                    {i.email}
+                                </td>
+                            </tr>
+                        ))}
+
                         
                     </tbody>
                 </table>
