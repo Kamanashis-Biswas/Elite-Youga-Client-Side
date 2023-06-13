459fff80e431c34378444f74d092f15b1678d8d5
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2023-06-13 12:07:11 +0600

    manage user component edit

--- src/pages/ManageUsers/ManageUsers.jsx
+++ src/pages/ManageUsers/ManageUsers.jsx
@@ -1,9 +1,24 @@
-
+import { useEffect, useRef, useState } from "react";
+import {useLocation, useNavigate} from 'react-router-dom';
 
 const ManageUsers = () => {
+    const [users, setUsers] = useState([]);
+    const location  = useLocation();
+    const navigate = useNavigate();
+
+    const updateRole = ({id, role})=>{
+        fetch(`http://localhost:5000/update-user?id=${id}&role=${role}`).then(()=>{});
+    };
+    useEffect(()=>{
+        fetch('http://localhost:5000/user')
+            .then(data=>data.json())
+            .then(data=>{
+                setUsers(data.users);
+            });
+
+    }, []);
     return (
         <div>
-            <h2>Manage Users</h2>
             <div>
                 <div className="overflow-x-auto">
                     <table className="table">
@@ -16,32 +31,33 @@ const ManageUsers = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
+                            {users.map(u=>(
+                            <tr key={u._id}>
                                 <td>
                                     <div className="flex items-center space-x-3">
                                         <div className="avatar">
                                             <div className="mask mask-squircle w-12 h-12">
-                                                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                                <img src={u.photo} alt="Avatar Tailwind CSS Component" />
                                             </div>
                                         </div>
                                         <div>
-                                            <div className="font-bold">Hart Hagerty</div>
+                                            <div className="font-bold">{u.name}</div>
                                         </div>
                                     </div>
                                 </td>
                                 <td>
-                                    Zemlak, Daniel and Leannon
-                                    <br />
+                                    {u.email}
                                 </td>
                                 <td>
                                     <div className="flex flex-col">
-                                        <button className="btn btn-sm mb-2">Admin</button>
-                                        <button className="btn btn-sm mb-2">Instructor</button>
-                                        <button className="btn btn-sm mb-2">Student</button>
+                                        <button onClick={()=>updateRole({id: u._id, role: "admin"})} className="btn btn-sm mb-2">Admin</button>
+                                        <button onClick={()=>updateRole({id: u._id, role: "instructors"})} className="btn btn-sm mb-2">Instructor</button>
+                                        <button onClick={()=>updateRole({id: u._id, role: "student"})} className="btn btn-sm mb-2">Student</button>
                                     </div>
                                 </td>
 
                             </tr>
+                            ))}
                         </tbody>
 
 
